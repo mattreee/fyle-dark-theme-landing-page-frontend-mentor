9d7746e7c7c8f6f655d3ef2cd40df8f66837cded
mattreee <fantinatomatheuss@gmail.com>
2022-02-22 19:47:34 -0400

    footer finished

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,30 +1,52 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import Logo from '../images/logo.svg';
 
+import IconLocation from '../images/icon-location.svg';
+import IconPhone from '../images/icon-phone.svg';
+import IconEmail from '../images/icon-email.svg';
+
+import SocialFace from '../images/face_white_24dp.svg';
+import SocialBird from '../images/thumb_up_white_24dp.svg';
+import SocialCam from '../images/photo_camera_white_24dp.svg';
+
 const Footer = () => {
   return (
-    <footer>
-      <img src={Logo} alt='' />
+    <footer className='footer'>
+      <img className='footer__logo' src={Logo} alt='' />
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-
-      <div>
-        <p>+1-543-123-4567</p>
-        <p>example@fylo.com</p>
+      <div className='footer__container'>
+        <address className='footer__address'>
+          <img src={IconLocation} alt='' />
+          <p className='footer__address-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+          <div className='footer__address-info'>
+            <p className='footer__address-phone'>
+              <img className='icon-info' src={IconPhone} alt='' />
+              +1-543-123-4567
+            </p>
+            <p className='footer__address-email'>
+              <img className='icon-info' src={IconEmail} alt='' />
+              example@fylo.com
+            </p>
+          </div>
+        </address>
+        <ul className='footer__nav'>
+          <li><a className='footer__nav-link' href='#'>About Us</a></li>
+          <li><a className='footer__nav-link' href='#'>Jobs</a></li>
+          <li><a className='footer__nav-link' href='#'>Press</a></li>
+          <li><a className='footer__nav-link' href='#'>Blog</a></li>
+        </ul>
+        <ul className='footer__nav'>
+          <li><a className='footer__nav-link' href='#'>Contact Us</a></li>
+          <li><a className='footer__nav-link' href='#'>Terms</a></li>
+          <li><a className='footer__nav-link' href='#'>Privacy</a></li>
+        </ul>
+        <ul className='footer__socials'>
+          <li><a href='#'><img src={SocialFace} alt='' /></a></li>
+          <li><a href='#'><img src={SocialBird} alt='' /></a></li>
+          <li><a href='#'><img src={SocialCam} alt='' /></a></li>
+        </ul>
       </div>
-
-      <ul>
-        <li>About Us</li>
-        <li>Jobs</li>
-        <li>Press</li>
-        <li>Blog</li>
-      </ul>
-
-      <ul>
-        <li>Contact Us</li>
-        <li>Terms</li>
-        <li>Privacy</li>
-      </ul>
     </footer>
   )
 }
